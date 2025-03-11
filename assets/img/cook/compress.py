15d76bf0e051d4130de04c9a7e0bd9a21397354d
prophethjk <prophethjk@gmail.com>
2025-03-11 20:15:41 +0800

    更新文章

--- assets/img/cook/compress.py
+++ assets/img/cook/compress.py
@@ -8,7 +8,7 @@ def compress_image(image_path, max_size_kb=500, quality_step=5):
     # 如果文件已经小于 500KB，则跳过
     if os.path.getsize(image_path) <= max_size:
         print(f"跳过: {image_path} (已小于 {max_size_kb}KB)")
-        return
+        return None
     
     # 确保是 RGB 模式（避免 PNG RGBA 影响压缩）
     if img.mode in ("RGBA", "P"):
@@ -18,21 +18,26 @@ def compress_image(image_path, max_size_kb=500, quality_step=5):
     temp_path = image_path + ".temp.jpg"  # 临时文件路径
     
     while quality > 10:
-        img.save(temp_path, "JPEG", quality=quality)
+        img.save(temp_path, "WEBP", quality=quality)
         if os.path.getsize(temp_path) <= max_size:
             break  # 达到目标大小
         quality -= quality_step
     
     # 替换原文件
-    os.replace(temp_path, image_path)
     print(f"压缩完成: {image_path} (质量 {quality})")
+    return temp_path
+
+def replace_image(src_img,dst_img):
+    os.replace(src_img, dst_img)
 
 def process_directory(directory):
     for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
                 image_path = os.path.join(root, file)
-                compress_image(image_path)
+                temp_path = compress_image(image_path)
+                if temp_path != None:
+                    replace_image(temp_path, image_path)
 
 if __name__ == "__main__":
     dir_path = os.getcwd()  # 直接使用当前目录
